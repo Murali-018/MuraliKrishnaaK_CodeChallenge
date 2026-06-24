edbbb9326841d7f9adf00d6875bf9988b28929ff
Murali Krishnaa K <muralikrishna01018@gmail.com>
2026-06-24 12:43:08 +0530

    player service modified

--- src/main/java/com/hexaware/springrest/cricket/service/PlayerServiceImpl.java
+++ src/main/java/com/hexaware/springrest/cricket/service/PlayerServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 public class PlayerServiceImpl implements IPlayerService {
 
     @Autowired
-    private PlayerRepository playerRepository;
+    private PlayerRepository playerRepository;hh
 
 	@Override
 	public PlayerDTO addPlayer(PlayerDTO playerDTO) {
